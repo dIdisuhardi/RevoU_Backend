ec1e8da5a84163035d0f4b7d70ead61d5a6c927d
DIdisuhardi <didisuhardi00@gmail.com>
2023-10-07 19:50:38 +0700

    debuging

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const materiRoutes = require('../routes/materiRoutes');
 
 app.use(cors());
 app.use(express.json());
